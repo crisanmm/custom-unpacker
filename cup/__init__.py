c5a2a1bd6d45d9f48a2f9d27bf0e80bee4a6808e
mcrisan5537 <crisanmihai5537@gmail.com>
2021-01-07 19:38:29 +0200

    __init__.py: Slight docstring modification

--- cup/__init__.py
+++ cup/__init__.py
@@ -1,5 +1,5 @@
 """
-Cup (Custom Unpacker-Packer) is a proprietary archiver without compression.
+Cup (Custom Unpacker-Packer) is an archiver without compression.
 
 Cup is used to pack multiple files or directories into a single .cup file known as an archive.
 Cup also supports functionality for getting information from such a .cup file as well as unpacking
