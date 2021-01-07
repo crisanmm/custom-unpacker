b6c4856d117cb9079f9952d0706896c6f2d02020
mcrisan5537 <crisanmihai5537@gmail.com>
2021-01-07 13:41:08 +0200

    exceptions.py: add more exceptions

--- cup/exceptions.py
+++ cup/exceptions.py
@@ -6,11 +6,40 @@ import os
 
 
 class CupException(Exception):
-    """ Base custom exception class """
+    """Base custom exception class."""
     pass
 
 
-class ArchiveExistsError(CupException):
-    """ Raised when """
+class ArchiveAlreadyExistsError(CupException):
+    """Raised when"""
+
     def __init__(self, archive_path):
-        super().__init__(f'Archive output file already exists: {os.path.abspath(archive_path)}')
+        super().__init__(f"Archive output file already exists: {os.path.abspath(archive_path)}")
+
+
+class ResourceNonExistentError(CupException):
+    """"""
+
+    def __init__(self, resource_path):
+        super().__init__(f"Resource doesn't exist: {resource_path}")
+
+
+class ResourceCantBeArchivedError(CupException):
+    """"""
+
+    def __init__(self, resource_path):
+        super().__init__(f"Resource is not a file/directory: {resource_path}")
+
+
+class ArchiveNonExistentError(CupException):
+    """"""
+
+    def __init__(self, archive_path):
+        super().__init__(f"Archive doesn't exist: {archive_path}")
+
+
+class ArchiveNotRecognizableError(CupException):
+    """"""
+
+    def __init__(self, archive_path):
+        super().__init__(f"File is not a CUP archvie: {archive_path}")
